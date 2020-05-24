7295ec3a0b322e30e6db3172100738f6b9d45969
akim157 <akim157@mail.ru>
2020-05-25 00:17:37 +0300

    three commit

---
+++ typescript/8/operator.ts
@@ -0,0 +1,20 @@
+interface Person {
+    name: string
+    age: number
+}
+
+type PersonKeys = keyof Person //'name' | 'age'
+let myName: PersonKeys = 'name'
+myName = 'age'
+
+type User = {
+    _id: number
+    name: string
+    email: string
+    createAt: Date
+}
+
+type UserKeysNoMeta1 = Exclude<keyof User, '_id' | 'createdAt'> // 'name' | 'email'
+type UserKeysNoMeta2 = Pick<User, 'name' | 'email'> //'name' | 'email'
+
+let u1: UserKeysNoMeta1 = 'name'
